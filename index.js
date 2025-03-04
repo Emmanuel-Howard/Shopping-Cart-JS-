d8468f6a535a691b3c2e8854a6152063bf5e0ac3
Emmanuel Howard <emmanuelhoward3000@gmail.com>
2025-03-03 21:51:08 -0500

    Constants & Variables

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 const h1 = document.getElementById("h1");
+const shoppingInput = document.getElementById("shoppingInput");
+const shoppingBtn = document.getElementById("shoppingBtn");
+
+let shoppingList = []
+
+// onclick function
